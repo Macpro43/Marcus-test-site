f79595425da84e62be70f0db2579eef13742efdd
marcus <mproietti43@gmail.com>
2025-04-09 09:28:51 -0700

    Delete src directory

--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer style={{ textAlign: 'center', padding: '20px', color: '#888' }}>
-      &copy; 2025 Retro Designer. All rights reserved.
-    </footer>
-  );
-}
